5fd6bcbc4ac1df6e768108b641456f67109ff8bd
AnuragPandey-19 <89677430+Anurag19-Pandey@users.noreply.github.com>
2023-09-20 10:27:59 +0530

    fourth commit

--- src/components/VideoC/Video.js
+++ src/components/VideoC/Video.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './VideoStyles.css' ;
+import VideoSrc from '../../assets/videos/video-background.mp4' ;
 
 const Video = () => {
 
 
   return (
     <div className='video-container'>
-    
-    </div>
+     <video className="video-design" controls >
+        <source src={VideoSrc} type="video/mp4"/>
+     </video>
+    </div>  
   )
 }
 
